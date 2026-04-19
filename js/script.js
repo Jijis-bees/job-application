1dfc7cd540cc1351b608ee0f391881142937ba3d
Jijis-bees <jijisbee@gmail.com>
2026-04-19 14:57:44 +0200

    logo insert + titel

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ setInterval(typeWriter, 1000);
 
 var i = 0;
 var txt = 'frontend-udvikler.'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var speed = 220; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
