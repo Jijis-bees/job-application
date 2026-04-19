234227bc0c01e11f7c6c6576b365d130a90328d3
Jijis-bees <jijisbee@gmail.com>
2026-04-19 16:08:44 +0200

    start page added

---
+++ js/start.js
@@ -0,0 +1,13 @@
+setInterval(typeWriter, 1000);
+
+var i = 0;
+var txt = 'frontend-udvikler? UI/UX Designer? multimediedesigner?'; /* The text */
+var speed = 250; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("start-txt").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
